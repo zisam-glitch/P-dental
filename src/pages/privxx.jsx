52d2a4fceb3ad70ddd6985bd90ac81dd136ec7f2
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-14 16:12:15 +0600

    commit

--- src/pages/privxx.jsx
+++ src/pages/privxx.jsx
@@ -1,49 +1,363 @@
-import Accordition from "../components/mobile-d";
-import { useTranslation, withTranslation } from "react-i18next";
+import React from "react";
+import Layout from "../components/global/Layout";
+import PageHeader from "../components/global/PageHeader";
+import { Helmet } from "react-helmet";
 
-const FAQ = function () {
-  const { t } = useTranslation("faqBox");
+const ParishDentalPracticePolicy = () => {
   return (
-    <div className="faq-section">
-      <div className="faq-header">
-        <p>{t("faq.header")}</p>
-      </div>
-      <div className="faq-container">
-        <div className="faq-row">
-          <div className="faq-column">
-            <Accordition title={t("faq.question1")} id="section_1">
-              <p>{t("faq.answer1")}</p>
-            </Accordition>
+    <Layout>
+      <Helmet>
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+          integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+          crossorigin="anonymous"
+        ></script>
+      </Helmet>
+      <PageHeader pageTitle="Parish Dental Practice Policy" />
+
+      <section id="faq" class="faq">
+        <div class="container">
+          <div style="text-align:center;">
+            <h2>Frequently Asked Questions</h2>
           </div>
-          <div className="faq-column">
-            <Accordition title={t("faq.question2")} id="section_2">
-              <p>{t("faq.answer2")}</p>
-            </Accordition>
-          </div>
-          <div className="faq-column">
-            <Accordition title={t("faq.question3")} id="section_3">
-              <p>{t("faq.answer3")}</p>
-            </Accordition>
-          </div>
-          <div className="faq-column">
-            <Accordition title={t("faq.question4")} id="section_4">
-              <p>{t("faq.answer4")}</p>
-            </Accordition>
-          </div>
-          <div className="faq-column">
-            <Accordition title={t("faq.question5")} id="section_5">
-              <p>{t("faq.answer5")}</p>
-            </Accordition>
-          </div>
-          <div className="faq-column">
-            <Accordition title={t("faq.question6")} id="section_6">
-              <p>{t("faq.answer6")}</p>
-            </Accordition>
+
+          <div class="faq-list">
+            <ul>
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  class="collapse"
+                  data-bs-target="#faq-list-1"
+                >
+                  What is lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-1"
+                  class="collapse show"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-2"
+                  class="collapsed"
+                >
+                  What is lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-2"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-3"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-3"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-4"
+                  class="collapsed"
+                >
+                  What is lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-4"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-5"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-5"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-6"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-6"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-7"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-7"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-8"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-8"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-9"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-9"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-10"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-10"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <i
+                  class="fa fa-question-circle icon-help"
+                  aria-hidden="true"
+                ></i>{" "}
+                <a
+                  data-bs-toggle="collapse"
+                  data-bs-target="#faq-list-11"
+                  class="collapsed"
+                >
+                  What is Lorem?{" "}
+                  <i
+                    class="fa fa-sm fa-angle-down icon-show"
+                    aria-hidden="true"
+                  ></i>
+                  <i class="fa fa-sm fa-angle-up icon-close"></i>
+                </a>
+                <div
+                  id="faq-list-11"
+                  class="collapse"
+                  data-bs-parent=".faq-list"
+                >
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </Layout>
   );
 };
 
-export default FAQ;
+export default ParishDentalPracticePolicy;
