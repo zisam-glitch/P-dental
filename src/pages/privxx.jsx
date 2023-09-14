bf87871b590a371a5fe9ab408c162d7a9a7b0e12
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-14 14:05:50 +0600

    commit

--- src/pages/privxx.jsx
+++ src/pages/privxx.jsx
@@ -6,6 +6,46 @@ const ParishDentalPracticePolicy = () => {
   return (
     <Layout>
       <PageHeader pageTitle="Parish Dental Practice Policy" />
+      <div>
+        <div tabindex="0" class="faqblock">
+          <input type="checkbox" id="faq" />
+          <label for="faq">
+            Can I accumulate pulses, and activate the output after a desired
+            number of pulses are received?
+          </label>
+          <div class="faqblock-content">
+            <p>
+              No. The PCM control modules will output for a set number of
+              seconds after every pulse is received. You will have to ensure
+              that the flow rate combined with the flow meter's pulse rate is
+              accounted for by ensuring the "on time" does not exceed the
+              available time between pulses. If you do need to accumulate
+              pulses, we recommend the SeaMetrics PT35 Pulse Timer. More
+              information can be found on the PT35{" "}
+              <a href="http://www.promagenviro.com/Counter-Timer">here</a>.
+            </p>
+          </div>
+        </div>
+        <div tabindex="0" class="faqblock">
+          <input type="checkbox" id="faq" />
+          <label for="faq">
+            Can I accumulate pulses, and activate the output after a desired
+            number of pulses are received?
+          </label>
+          <div class="faqblock-content">
+            <p>
+              No. The PCM control modules will output for a set number of
+              seconds after every pulse is received. You will have to ensure
+              that the flow rate combined with the flow meter's pulse rate is
+              accounted for by ensuring the "on time" does not exceed the
+              available time between pulses. If you do need to accumulate
+              pulses, we recommend the SeaMetrics PT35 Pulse Timer. More
+              information can be found on the PT35{" "}
+              <a href="http://www.promagenviro.com/Counter-Timer">here</a>.
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
