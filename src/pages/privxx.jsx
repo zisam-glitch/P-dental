a23820e615765b12ac39046822d753618f091a56
zisam shaikh <zisamshaikh7@gmail.com>
2023-09-15 00:28:10 +0600

    cc

--- src/pages/privxx.jsx
+++ src/pages/privxx.jsx
@@ -3,60 +3,10 @@ import Layout from "../components/global/Layout";
 import PageHeader from "../components/global/PageHeader";
 
 const ParishDentalPracticePolicy = () => {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
   return (
     <Layout>
       <PageHeader pageTitle="Parish Dental Practice Policy" />
-      <>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style
-          dangerouslySetInnerHTML={{
-            __html:
-              "\n.accordion {\n  background-color: #eee;\n  color: #444;\n  cursor: pointer;\n  padding: 18px;\n  width: 100%;\n  border: none;\n  text-align: left;\n  outline: none;\n  font-size: 15px;\n  transition: 0.4s;\n}\n\n.active, .accordion:hover {\n  background-color: #ccc; \n}\n\n.panel {\n  padding: 0 18px;\n  display: none;\n  background-color: white;\n  overflow: hidden;\n}\n",
-          }}
-        />
-        <h2>Accordion</h2>
-        <button className="accordion">Section 1</button>
-        <div className="panel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <button className="accordion">Section 2</button>
-        <div className="panel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <button className="accordion">Section 3</button>
-        <div className="panel">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </>
+    
     </Layout>
   );
 };
